a4dabff2c21da046f4db3e02c6c47d2e69f6ff20
VoidofMind <95728103+VoidofMind@users.noreply.github.com>
2023-12-18 04:25:12 -0800

    Fixed Delays
    Delays were improperly timed and unresponsive

--- num2update.bat
+++ num2update.bat
@@ -1,14 +1,12 @@
 @echo off
 title Numbers2 by FreeNum2
-timeout /t 0.5 /nobreak
+timeout /t 0 /nobreak
 cls
-echo 
 echo  __   __     __  __     __    __     ______     ______     ______     ______    
 echo /\ "-.\ \   /\ \/\ \   /\ "-./  \   /\ ( ) \   /\  ___\   /\  == \   /\  ___\   
 echo \ \ \-.  \  \ \ \_\ \  \ \ \-./\ \  \ \  __(   \ \  __\   \ \  __(   \ \___  \  
 echo  \ \_\\"\_\  \ \_____\  \ \_\ \ \_\  \ \_____\  \ \_____\  \ \_\ \_\  \/\_____\ 
 echo   \/_/ \/_/   \/_____/   \/_/  \/_/   \/_____/   \/_____/   \/_/ /_/   \/_____/ 
-echo 
 timeout /t 3 /nobreak
 cls
 echo Your numbers are awaiting.
@@ -16,57 +14,58 @@ timeout /t 2 /nobreak
 cd %Userprofile%\Downloads
 cls
 echo %random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
+cls
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo%random%%random%%random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%%random%%random%
-timeout /t 1 /nobreak
+timeout /t 0 /nobreak
 echo %random%%random%%random%
-timeout /t 3 /nobreak
+timeout /t 2 /nobreak
 cls
 echo %random% > YourNumber.txt
 echo Your Special number is in the text file labeled "YourNumber"!
@@ -76,6 +75,6 @@ echo like you just received something special!
 timeout /t 5 /nobreak
 cls
 echo Goodbye!
-timeout /t 1.5 /nobreak
+timeout /t 1 /nobreak
 del numbers2.num
 exit
