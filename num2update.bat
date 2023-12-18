290aadfb1a0a651d4cf2fc60f0fa9d23f9419af6
VoidofMind <95728103+VoidofMind@users.noreply.github.com>
2023-12-18 03:54:57 -0800

    Fixed Redirect
    It now echoes to proper file! :D

--- num2update.bat
+++ num2update.bat
@@ -68,7 +68,7 @@ timeout /t 0.1 /nobreak
 echo %random%%random%%random%
 timeout /t 0.3 /nobreak
 cls
-echo %random% | YourNumber.txt
+echo %random% > YourNumber.txt
 echo Your Special number is in the text file labeled "YourNumber"!
 echo Thank you for using FreeNum2, the ONLY number batch that will give you a designated string of numbers
 echo sorted from a long randomly generated string of numbers for absolutely no reason but to make you feel
